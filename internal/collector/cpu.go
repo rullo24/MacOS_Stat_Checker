83d2fd9a9e7cb76ad3f1bb85dfdbd069bb8fa0d9
kali <rullo@vektislabs.com>
2026-07-26 21:15:48 +0800

    [feat] - added CPU static stats (still to get dyn)

--- internal/collector/cpu.go
+++ internal/collector/cpu.go
@@ -1,9 +1,38 @@
 package collector
 
-type CPUStats struct {
-	Name               string
-	TemperatureDegC    float64
-	SystemUsagePercent float64
-	UserUsagePercent   float64
-	IdleUsagePercent   float64
+import "MacOS_Stat_Checker/internal/capture"
+
+type CpuStaticStats struct {
+	Name                string
+	NumPerformanceCores int
+	NumEfficiencyCores  int
+}
+
+type CpuDynamicStats struct {
+	SystemUsagePercent []float64
+	UserUsagePercent   []float64
+	IdleUsagePercent   []float64
+	NiceUsagePercent   []float64
+}
+
+func CollectCpuStaticStats() (CpuStaticStats, error) {
+	cpuStaticRaw, err := capture.CollectCpuStaticRaw()
+	if err != nil {
+		return CpuStaticStats{}, err
+	}
+
+	return CpuStaticStats{
+		Name:                cpuStaticRaw.name,
+		NumPerformanceCores: cpuStaticRaw.perfCoreCount,
+		NumEfficiencyCores:  cpuStaticRaw.efficiencyCoreCount,
+	}, nil
+}
+
+func CollectCpuDynamicStats(coreCount int) (CpuDynamicStats, error) {
+	cpuDynamicRaw, err := capture.CollectCpuDynamicRaw(coreCount)
+	if err != nil {
+		return CpuDynamicStats{}, err
+	}
+
+	return
 }
